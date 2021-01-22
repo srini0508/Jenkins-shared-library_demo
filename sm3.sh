6d4eeb9e7bde23e68be3edc01f24df6ada660013
CloudSeals <48088536+srini0508@users.noreply.github.com>
2021-01-22 11:58:18 +0530

    Update sm3.sh

--- sm3.sh
+++ sm3.sh
@@ -15,3 +15,27 @@ git_version=$(git describe --tags $l_tag)
 oldversion=$git_version
 
 echo "1. old version is $oldversion"
+
+sm_increment() {
+  local increment_type=$1
+  local current_version=$( echo $oldversion | tr -dc '0-9.' )
+  echo "2. sm_increment_without v $current_version"
+  local version_array=( ${current_version//./ } )
+  case $increment_type in
+    "major" )
+      ((version_array[0]++))
+      version_array[1]=0
+      version_array[2]=0
+      ;;
+    "minor" )
+      ((version_array[1]++))
+      version_array[2]=0
+      ;;
+    "patch" )
+      ((version_array[2]++))
+      ;;
+  esac
+
+  echo "${version_array[0]}.${version_array[1]}.${version_array[2]}"
+}
+
