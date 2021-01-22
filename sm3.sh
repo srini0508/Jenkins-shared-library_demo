53d6c3ad48869a036a9999c9960e9b1e4e02132d
CloudSeals <48088536+srini0508@users.noreply.github.com>
2021-01-22 12:20:08 +0530

    Update sm3.sh

--- sm3.sh
+++ sm3.sh
@@ -14,18 +14,72 @@ l_tag=$(git rev-list --tags --max-count=1)
 git_version=$(git describe --tags $l_tag)
 oldversion=$git_version
 
+
+get_increment_semantic_type_from_git() {
+  local latest_commit_message="$(get_commit_message_from_git)"
+  echo $(get_increment_semantic_type_from_string "$latest_commit_message")
+}
+
+get_release_message_from_git() {
+  echo $(get_commit_message_from_git)
+}
+
+get_commit_message_from_git() {
+  echo $(git log -1 --pretty=%B)
+}
+
+get_increment_semantic_type_from_string() {
+  local message="$1"
+  if [[ $message == *"[major]"* ]]; then
+      echo "major"
+  elif [[ $message == *"[minor]"* ]]; then
+      echo "minor"
+  else
+      echo "patch"
+  fi
+}
+
+
 echo "1. old version is $oldversion"
 
-while getopts ":Mmp" Option
-do
-  case $Option in
-    M ) major=true;;
-    m ) minor=true;;
-    p ) patch=true;;
+while getopts ":i:o:t:p:f:l:m:b:" opt; do
+  case ${opt} in
+    i )
+      version_input=$OPTARG
+      ;;
+    o )
+      version_output=$OPTARG
+      ;;
+    t )
+      version_increment_type=$OPTARG
+      ;;
+    p )
+      version_prefix=$OPTARG
+      ;;
+    l )
+      version_file_language=$OPTARG
+      ;;
+    f )
+      version_file=$OPTARG
+      ;;
+    m )
+      version_message=$OPTARG
+      ;;
+    b )
+      version_bash_file=$OPTARG
+      ;;
+    \? )
+      echo "Invalid option: $OPTARG" 1>&2
+      ;;
+    : )
+      echo "Invalid option: $OPTARG requires an argument" 1>&2
+      ;;
   esac
 done
+shift $((OPTIND -1))
 
-shift $(($OPTIND - 1))
+
+
 current_version=$( echo $oldversion | tr -dc '0-9.' )
 version=$current_version
 
