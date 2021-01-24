cd081a032e29d65c0212c866bd831b255364eb93
CloudSeals <48088536+srini0508@users.noreply.github.com>
2021-01-24 11:00:38 +0530

    Update newsm.sh

--- newsm.sh
+++ newsm.sh
@@ -8,11 +8,12 @@ version_file_language="go"
 version_file="./version_info"
 version_message=""
 version_bash_file="./version_env.sh"
-oldversion=$(cat version_info)
 
+echo "2222222222222222222"
 l_tag=$(git rev-list --tags --max-count=1)
 git_version=$(git describe --tags $l_tag)
 oldversion=$git_version
+echo $oldversion
 
 
 
